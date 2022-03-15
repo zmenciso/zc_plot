4ea3f619c186c7436147677a823717b06c05d8e5
Zephan M. Enciso <zenciso@nd.edu>
2022-03-15 11:30:54 -0400

    Wave ingest flow

--- cadence_plot.py
+++ cadence_plot.py
@@ -5,8 +5,10 @@
 
 import sys
 import os
+import re
 import pandas as pd
-from plot_functions import *
+import numpy as np
+
 
 # Globals
 
@@ -14,14 +16,18 @@ PROJ_DIR = os.path.dirname(os.path.realpath(__file__))
 FUNC_DIR = PROJ_DIR + '/plot_functions'
 CWD = os.getcwd()
 
+for entry in os.scandir(FUNC_DIR):
+    if entry.is_file() and os.path.splitext(entry)[-1] == '.py':
+        exec(f'from {os.path.basename(FUNC_DIR)} import {os.path.splitext(os.path.basename(entry))[0]}')
+
 PLOT = None
 INPUT = None
 VERBOSE = False
 SUMMARY = False
 FILETYPE = 'svg'
 
-
 # Functions
+
 
 def usage(exitcode):
     print(f'''{sys.argv[0]} [OPTIONS] PLOT INPUT [kwargs]
@@ -31,8 +37,13 @@ def usage(exitcode):
 
 PLOT is the plot you wish to create, defined in `plot_functions.py`:''')
 
-    for func in os.listdir(FUNC_DIR):
-        print(f'    {os.path.splitext(func)[0]}')
+    for func in os.scandir(FUNC_DIR):
+        if func.is_file() and os.path.splitext(entry)[-1] == '.py':
+            print(f'    {os.path.splitext(os.path.basename(func))[0]}')
+
+    print('''
+INPUT is the input data file, e.g. `data/my_data.csv`.
+If no INPUT and no kwargs are given, prints the usage for the specified PLOT.''')
 
     sys.exit(exitcode)
 
@@ -42,7 +53,19 @@ def ingest_wave(filename):
         print('ERROR: Input file must be .csv', file=sys.stderr)
         sys.exit(3)
 
-    df = pd.read_csv(filename)
+    df_in = pd.read_csv(filename)
+    df = pd.DataFrame()
+
+    x = df_in.iloc[:, 0]
+    for label, content in df_in.iloc[:, 1::2].iteritems():
+        param = re.findall(r".+ \((.*)\) .+", label)[0].split(",")
+        d_fill = pd.DataFrame(np.array([x, content]).T,
+                              columns=['x', label.split()[0]])
+        for term in param:
+            d_fill[term.split('=')[0]] = np.repeat(float(term.split('=')[1]),
+                                                   len(d_fill['x']))
+
+        df = pd.concat([df, d_fill], axis=0)
 
     return df
 
@@ -69,8 +92,17 @@ if __name__ == '__main__':
 
         args.pop(0)
 
-    if len(args) < 2:
+    # No arguments
+    if len(args) < 1:
         usage(2)
+
+    # Specific PLOT usage
+    elif len(args) == 1:
+        PLOT = args.pop(0)
+        eval(f'{PLOT}.usage()')
+        sys.exit(0)
+
+    # Normal
     else:
         PLOT = args.pop(0)
         INPUT = args.pop(0)
@@ -82,3 +114,4 @@ if __name__ == '__main__':
         df = ingest_wave(INPUT)
 
     eval(f'{PLOT}.plot(df, kwargs)')
+    sys.exit(0)
