87e6f25833d3e87b2d8fba1e4bebf6921317e7df
Zephan M. Enciso <zenciso@nd.edu>
2022-03-16 13:11:20 -0400

    Reduce cyclomatic complexity

--- plot_functions/replot.py
+++ plot_functions/replot.py
@@ -4,7 +4,6 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
 
 
 def usage():
@@ -13,6 +12,7 @@ def usage():
     hue=str         h=str       Change hue (WARNING default: None)
     style=str       s=str       Specify style (default: None)
     size=tuple                  Change figsize (default: 18/3,19/3)
+    msize=float     ms=float    Change marker size for scatter (default: 10)
     alpha=float     a=float     Change alpha (default: 0.5)
     logx=bool       lx=bool     Enable/disable log for x-axis (default: False)
     logy=bool       ly=bool     Enable/disable log for y-axis (default: False)
@@ -21,7 +21,7 @@ def usage():
     ylabel=str      yl=str      Change y axis label (default: y)
     ltitle=str      lt=str      Change legend title (default: hue)
     xlim=tuple                  Change xlim (default: full range)
-    p_type=str      pt=str      Change the plot type (default: 'line')
+    ptype=str       pt=str      Change the plot type (default: 'line')
     filetype=str    ft=str      Change filetype (default: 'svg')
     filename=str    fn=str      Custom filename''')
 
@@ -37,9 +37,10 @@ def key_expander(key):
             'xl': 'xlabel',
             'yl': 'ylabel',
             'lt': 'ltitle',
-            'pt': 'p_type',
+            'pt': 'ptype',
             'ft': 'filetype',
-            'fn': 'filename'
+            'fn': 'filename',
+            'ms': 'msize'
             }
 
     if key in conversion:
@@ -52,7 +53,7 @@ def plot(df, kwargs):
     # TODO: Fix sig figs on legend!
 
     param = {
-        'figsize': (18 / 3, 9 / 3),
+        'figsize': '6,3',
         'alpha': 0.8,
         'logy': False,
         'logx': False,
@@ -64,47 +65,53 @@ def plot(df, kwargs):
         'y': df.columns[1],
         'hue': None,
         'style': None,
-        'p_type': 'line',
+        'ptype': 'line',
+        'msize': 10
     }
 
     param['ylabel'] = param['y']
     param['l_title'] = param['hue']
-    param['xlim'] = (df['x'].iloc[0], df['x'].iloc[-1])
+    param['xlim'] = (f"{df['x'].iloc[0]},{df['x'].iloc[-1]}")
 
     for arg in kwargs:
         key, value = arg.split('=')
         key = key_expander(key)
         if key in param:
             param[key] = value
-        else:
-            print(f'ERROR: Unkown kwarg {key}', file=sys.stderr)
+
+    param['figsize'] = tuple(map(float, param['figsize'].split(',')))
+    param['alpha'] = float(param['alpha'])
+    param['logy'] = bool(param['logy'])
+    param['logx'] = bool(param['logx'])
+    param['bbox'] = bool(param['bbox'])
+    param['xlim'] = tuple(map(float, param['xlim'].split(',')))
 
     if param['palette']:
         sns.set_palette(sns.color_palette(param['palette']))
 
-    plt.figure(figsize=tuple(map(float, param['figsize'].split(','))))
+    plt.figure(figsize=param['figsize'])
 
     if param['hue']:
         df[param['hue']] = df[param['hue']].astype(float)
 
-    if param['p_type'] == 'line':
+    if param['ptype'] == 'line':
         ax = sns.lineplot(data=df, x='x', y=param['y'], hue=param['hue'],
-                style=param['style'], alpha=float(param['alpha']))
-    elif param['p_type'] == 'scatter':
+                          style=param['style'], alpha=param['alpha'])
+    elif param['ptype'] == 'scatter':
         ax = sns.scatterplot(data=df, x='x', y=param['y'], hue=param['hue'],
-                style=param['style'], alpha=float(param['alpha']),
-                edgecolor=None, s=10)
+                             style=param['style'], alpha=param['alpha'],
+                             edgecolor=None, s=param['msize'])
 
     plt.xlabel(param['xlabel'])
     plt.ylabel(param['ylabel'])
 
-    plt.xlim(tuple(map(float, param['xlim'].split(','))))
-    if bool(param['logx']):
+    plt.xlim(param['xlim'])
+    if param['logx']:
         ax.set_xscale('log')
-    if bool(param['logy']):
+    if param['logy']:
         ax.set_yscale('log')
 
-    if bool(param['bbox']) and (param['hue'] or param['style']):
+    if param['bbox'] and (param['hue'] or param['style']):
         handles, labels = plt.gca().get_legend_handles_labels()
         plt.legend(handles,
                    labels,
