76e5762a24f840b2c6c187544e638077548b2352
Zephan M. Enciso <zenciso@nd.edu>
2022-03-16 13:11:46 -0400

    Call replot internally

--- plot_functions/inputrefnoise.py
+++ plot_functions/inputrefnoise.py
@@ -1,12 +1,9 @@
 # Zephan M. Enciso
 # Intelligent MicroSystems Lab
 
-import matplotlib.pyplot as plt
-from datetime import datetime
+from plot_functions import replot
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import sys
 
 
 def usage():
@@ -14,104 +11,47 @@ def usage():
     fs=float        Set sample rate in Hz (default: 50e6)
     Ts=float        Set sample period in s, overrides fs (default: 1/fs)
     delay=float     Set delay before first sample (default: 0)
-    s=float         Set size of point (default: 10)
-    size=tuple      Change figsize (default: 18/3,19/3)
-    alpha=float     Change alpha (default: 0.5)
-    bbox=bool       Enable/disable bbox for legend (default: True)
-    xlabel=str      Change x axis label (default: 'x')
-    ylabel=str      Change y axis label (default: y)
-    xlim=tuple      Change xlim (default: full range)
-    filetype=str    Change filetype (default: svg)
-    filename=str    Custom filename''')
+    Uses the same plotting kwargs as replot''')
 
 
 def plot(df, kwargs):
-    fs = 50e6
-    Ts = None
-    figsize = (18/3, 9/3)
-    alpha = 0.8
-    bbox = False
-    filetype = 'svg'
-    filename = None
-    s = 10
-
-    xlim = (df['x'].iloc[0], df['x'].iloc[-1])
-    y = df.columns[1]
-    bins = df.columns[2]
-    xlabel = bins
-    ylabel = y
-    delay = 9e-9
+    param = {
+        'fs': 50e6,
+        'Ts': None,
+        'y': df.columns[1],
+        'bins': df.columns[2],
+        'delay': 9e-9
+        }
 
     for arg in kwargs:
         key, value = arg.split('=')
+        if key in param:
+            param[key] = value
 
-        if key == 'fs':
-            fs = float(value)
-        elif key == 'delay':
-            delay = float(value)
-        elif key == 'Ts':
-            Ts = float(value)
-        elif key == 'delay':
-            delay = float(value)
-        elif key == 's':
-            s = float(value)
-        elif key == 'size':
-            figsize = tuple(map(float, value.split(',')))
-        elif key == 'alpha':
-            alpha = float(value)
-        elif key == 'bbox':
-            bbox = bool(value)
-        elif key == 'xlabel':
-            xlabel = value
-        elif key == 'ylabel':
-            ylabel = value
-        elif key == 'xlim':
-            xlim = tuple(map(float, value.split(',')))
-        elif key == 'filetype':
-            filetype = value
-        elif key == 'filename':
-            filename = value
-        else:
-            print(f'ERROR: Unknown kwarg {key}', file=sys.stderr)
+    param['fs'] = float(param['fs'])
+    param['delay'] = float(param['delay'])
 
-    plt.figure(figsize=figsize)
-
-    if not Ts:
-        Ts = 1 / fs
+    if not param['Ts']:
+        param['Ts'] = 1 / param['fs']
+    else:
+        param['Ts'] = float(param['Ts'])
 
     d_fill = pd.Series(np.empty(shape=(0), dtype=np.float64))
 
     for i in range(0, len(df), size := np.unique(df['x']).size):
-        time = Ts + delay
+        time = param['Ts'] + param['delay']
         samples = list()
         for index, series in df.iloc[i:i+size].iterrows():
-            hue = series[bins]
+            hue = series[param['bins']]
             if series['x'] >= time:
-                samples.append(series[y])
-                time += Ts
+                samples.append(series[param['y']])
+                time += param['Ts']
 
-        d_fill = pd.concat([d_fill, pd.Series([np.mean(samples), hue])],
-                axis=1, ignore_index=True)
+        d_fill = pd.concat([d_fill, pd.Series([hue, np.mean(samples)])],
+                           axis=1, ignore_index=True)
 
-    pd_sampled = pd.DataFrame(d_fill.T.values, columns=[y, bins])
-    sns.scatterplot(data=pd_sampled, x=bins, y=y, alpha=alpha, edgecolor=None, s=s)
+    pd_sampled = pd.DataFrame(d_fill.T.values, columns=['x',
+                              param['y']]).iloc[1:, :]
 
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    if bbox:
-        handles, labels = plt.gca().get_legend_handles_labels()
-        plt.legend(handles,
-                   labels,
-                   loc='upper left',
-                   bbox_to_anchor=(1.02, 1),
-                   title=hue,
-                   borderaxespad=0)
-
-    plt.tight_layout()
-    if filename:
-        plt.savefig(f'{filename}.{filetype}')
-    else:
-        plt.savefig(
-            f'{y}_{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}.{filetype}')
-    plt.xlim(xlim)
+    kwargs += [f'y={param["y"]}', 'pt=scatter']
+    replot.plot(pd_sampled, kwargs)
