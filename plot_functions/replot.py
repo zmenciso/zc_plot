a5c8dae686cb16ec90a0b04b1dfb883c51d7a108
Zephan M. Enciso <zenciso@nd.edu>
2022-03-16 12:27:29 -0400

    Reduce cyclomatic complexity

--- plot_functions/replot.py
+++ plot_functions/replot.py
@@ -9,101 +9,113 @@ import sys
 
 def usage():
     print('''replot INPUT [kwargs]
-    y=str           Change y (default: second column)
-    hue=str         Change hue (WARNING default: None)
-    style=str       Specify style (default: None)
-    figsize=tuple   Change figsize (default: 18/3,19/3)
-    alpha=float     Change alpha (default: 0.5)
-    logx=bool       Enable/disable log for x-axis (default: False)
-    logy=bool       Enable/disable log for y-axis (default: False)
-    bbox=bool       Enable/disable bbox for legend (default: True)
-    xlabel=str      Change x axis label (default: 'x')
-    ylabel=str      Change y axis label (default: y)
-    xlim=tuple      Change xlim (default: full range)
-    filetype=str    Change filetype (default: svg)
-    filename=str    Custom filename''')
+    y=str                       Change y (default: second column)
+    hue=str         h=str       Change hue (WARNING default: None)
+    style=str       s=str       Specify style (default: None)
+    size=tuple                  Change figsize (default: 18/3,19/3)
+    alpha=float     a=float     Change alpha (default: 0.5)
+    logx=bool       lx=bool     Enable/disable log for x-axis (default: False)
+    logy=bool       ly=bool     Enable/disable log for y-axis (default: False)
+    bbox=bool       bb=bool     Enable/disable bbox for legend (default: True)
+    xlabel=str      xl=str      Change x axis label (default: 'x')
+    ylabel=str      yl=str      Change y axis label (default: y)
+    ltitle=str      lt=str      Change legend title (default: hue)
+    xlim=tuple                  Change xlim (default: full range)
+    p_type=str      pt=str      Change the plot type (default: 'line')
+    filetype=str    ft=str      Change filetype (default: 'svg')
+    filename=str    fn=str      Custom filename''')
+
+
+def key_expander(key):
+    conversion = {
+            'h': 'hue',
+            's': 'style',
+            'a': 'alpha',
+            'lx': 'logx',
+            'ly': 'logy',
+            'bb': 'bbox',
+            'xl': 'xlabel',
+            'yl': 'ylabel',
+            'lt': 'ltitle',
+            'pt': 'p_type',
+            'ft': 'filetype',
+            'fn': 'filename'
+            }
+
+    if key in conversion:
+        return conversion[key]
+    else:
+        return key
 
 
 def plot(df, kwargs):
     # TODO: Fix sig figs on legend!
 
-    figsize = (18 / 3, 9 / 3)
-    alpha = 0.8
-    logy = False
-    logx = False
-    bbox = True
-    filetype = 'svg'
-    filename = None
-    xlabel = 'x'
-    palette = None
+    param = {
+        'figsize': (18 / 3, 9 / 3),
+        'alpha': 0.8,
+        'logy': False,
+        'logx': False,
+        'bbox': True,
+        'filetype': 'svg',
+        'filename': None,
+        'xlabel': 'x',
+        'palette': None,
+        'y': df.columns[1],
+        'hue': None,
+        'style': None,
+        'p_type': 'line',
+    }
 
-    xlim = (df['x'].iloc[0], df['x'].iloc[-1])
-    y = df.columns[1]
-    hue = None
-    style = None
-    ylabel = y
+    param['ylabel'] = param['y']
+    param['l_title'] = param['hue']
+    param['xlim'] = (df['x'].iloc[0], df['x'].iloc[-1])
 
     for arg in kwargs:
         key, value = arg.split('=')
-
-        if key == 'y':
-            y = value
-        elif key == 'hue':
-            hue = value
-        elif key == 'style':
-            style = value
-        elif key == 'size':
-            figsize = tuple(map(float, value.split(',')))
-        elif key == 'alpha':
-            alpha = float(value)
-        elif key == 'logy':
-            logy = bool(value)
-        elif key == 'logx':
-            logx = bool(value)
-        elif key == 'filetype':
-            filetype = value
-        elif key == 'filename':
-            filename = value
-        elif key == 'bbox':
-            bbox = bool(value)
-        elif key == 'xlabel':
-            xlabel = value
-        elif key == 'ylabel':
-            ylabel = value
-        elif key == 'xlim':
-            xlim = tuple(map(float, value.split(',')))
+        key = key_expander(key)
+        if key in param:
+            param[key] = value
         else:
-            print(f'ERROR: Unknown kwarg {key}', file=sys.stderr)
+            print(f'ERROR: Unkown kwarg {key}', file=sys.stderr)
 
-    if palette:
-        sns.set_palette(sns.color_palette(palette))
-    plt.figure(figsize=figsize)
+    if param['palette']:
+        sns.set_palette(sns.color_palette(param['palette']))
 
-    if hue:
-        df[hue] = df[hue].astype(float)
+    plt.figure(figsize=tuple(map(float, param['figsize'].split(','))))
 
-    ax = sns.lineplot(data=df, x='x', y=y, hue=hue, style=style, alpha=alpha)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    if param['hue']:
+        df[param['hue']] = df[param['hue']].astype(float)
 
-    plt.xlim(xlim)
-    if logx:
+    if param['p_type'] == 'line':
+        ax = sns.lineplot(data=df, x='x', y=param['y'], hue=param['hue'],
+                style=param['style'], alpha=float(param['alpha']))
+    elif param['p_type'] == 'scatter':
+        ax = sns.scatterplot(data=df, x='x', y=param['y'], hue=param['hue'],
+                style=param['style'], alpha=float(param['alpha']),
+                edgecolor=None, s=10)
+
+    plt.xlabel(param['xlabel'])
+    plt.ylabel(param['ylabel'])
+
+    plt.xlim(tuple(map(float, param['xlim'].split(','))))
+    if bool(param['logx']):
         ax.set_xscale('log')
-    if logy:
+    if bool(param['logy']):
         ax.set_yscale('log')
 
-    if bbox and (hue or style):
+    if bool(param['bbox']) and (param['hue'] or param['style']):
         handles, labels = plt.gca().get_legend_handles_labels()
         plt.legend(handles,
                    labels,
                    loc='upper left',
                    bbox_to_anchor=(1.02, 1),
-                   title=hue,
+                   title=param['l_title'],
                    borderaxespad=0)
 
     plt.tight_layout()
-    if filename:
-        plt.savefig(f'{filename}.{filetype}')
+    if param['filename']:
+        plt.savefig(f'{param["filename"]}.{param["filetype"]}')
     else:
         plt.savefig(
-            f'{y}_{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}.{filetype}')
+            f'{param["y"]}_{datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}.{param["filetype"]}')
