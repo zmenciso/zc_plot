bf78110468c3470db015193e18e67cf84a0bda8b
Zephan M. Enciso <zenciso@nd.edu>
2022-03-18 11:25:44 -0400

    Add kdeplot and histplot (also histkde)

--- plot_functions/replot.py
+++ plot_functions/replot.py
@@ -11,16 +11,17 @@ def usage():
     print('''replot [-s] INPUT [kwargs]
     x=str                       Change x (default: first column)
     y=str                       Change y (default: second column)
-    hue=str         h=str       Change hue (WARNING default: None)
+    hue=str         h=str       Specify hue (WARNING default: None)
     style=str       s=str       Specify style (default: None)
-    size=tuple                  Change figsize (default: '6,3')
-    msize=float     ms=float    Change marker size for scatter (default: 10)
+    size=str                    Specify size (default: None)
+    figsize=tuple   fs=tuple    Change figsize (default: '6,3')
+    width=float     w=float     Change marker or line width (default: Depends)
     alpha=float     a=float     Change alpha (default: 0.5)
     palette=str     c=str       Palette, accepts cubehelix (default: 'crest')
     logx=bool       lx=bool     Enable/disable log for x-axis (default: False)
     logy=bool       ly=bool     Enable/disable log for y-axis (default: False)
     bbox=bool       bb=bool     Enable/disable bbox for legend (default: True)
-    xlabel=str      xl=str      Change x axis label (default: 'x')
+    xlabel=str      xl=str      Change x axis label (default: x)
     ylabel=str      yl=str      Change y axis label (default: y)
     ltitle=str      lt=str      Change legend title (default: automatic)
     xlim=tuple                  Change xlim (default: full range)
@@ -28,6 +29,11 @@ def usage():
     ptype=str       pt=str      Change the plot type (default: 'line')
     axes=str        ax=str      Change axes style (default: 'whitegrid')
     context=str     cx=str      Scale plot elements (default: 'notebook')
+    ci=float                    Change confidence interval size (default: 95)
+    stat=str                    Change stat/estimator (default: Depends)
+    bins=int                    Change number of bins/levels (default: 10)
+    fill=bool                   Enable/disable fill (default: Depends)
+    multiple=str                Change multiple behavior (default: 'layer')
     filetype=str    ft=str      Change filetype (default: 'svg')
     filename=str    fn=str      Custom filename''')
 
@@ -47,7 +53,8 @@ def key_expander(key):
         'pt': 'ptype',
         'ft': 'filetype',
         'fn': 'filename',
-        'ms': 'msize',
+        'w': 'width',
+        'fs': 'figsize',
         'ax': 'axes',
         'cx': 'context'
     }
@@ -70,7 +77,8 @@ def plot(df, kwargs):
         'filetype': 'svg',
         'filename': None,
         'ltitle': None,
-        'xlabel': 'x',
+        'xlabel': None,
+        'ylabel': None,
         'xlim': None,
         'ylim': None,
         'axes': 'whitegrid',
@@ -80,11 +88,15 @@ def plot(df, kwargs):
         'y': df.columns[1],
         'hue': None,
         'style': None,
+        'size': None,
         'ptype': 'line',
-        'msize': 10
+        'width': None,
+        'ci': 95,
+        'bins': 10,
+        'fill': False,
+        'stat': None,
+        'multiple': 'layer'
     }
-
-    param['ylabel'] = param['y']
 
     for arg in kwargs:
         key, value = arg.split('=')
@@ -94,10 +106,18 @@ def plot(df, kwargs):
 
     param['figsize'] = tuple(map(float, param['figsize'].split(',')))
     param['alpha'] = float(param['alpha'])
-    param['msize'] = float(param['msize'])
+    param['width'] = float(param['width']) if param['width'] else None
+    param['ci'] = float(param['ci'])
     param['logy'] = bool(param['logy'])
     param['logx'] = bool(param['logx'])
     param['bbox'] = bool(param['bbox'])
+    param['fill'] = bool(param['fill'])
+    param['bins'] = int(param['bins'])
+
+    if not param['ylabel']:
+        param['ylabel'] = param['y']
+    if not param['xlabel']:
+        param['xlabel'] = param['x']
 
     if not param['ltitle']:
         param['ltitle'] = f"{param['hue']}/{param['style']}" if param[
@@ -113,24 +133,53 @@ def plot(df, kwargs):
     sns.set_style(param['axes'])
     sns.set_context(param['context'])
 
-    if param['ptype'] == 'line':
+    if 'line' in param['ptype']:
         ax = sns.lineplot(data=df,
                           x=param['x'],
                           y=param['y'],
                           hue=param['hue'],
                           style=param['style'],
+                          size=param['size'],
                           alpha=param['alpha'],
+                          estimator=param['stat'] if param['stat'] else 'mean',
+                          lw=param['width'] if param['width'] else 2,
                           palette=cmap)
-    elif param['ptype'] == 'scatter':
+
+    elif 'scatter' in param['ptype']:
         ax = sns.scatterplot(data=df,
                              x=param['x'],
                              y=param['y'],
                              hue=param['hue'],
                              style=param['style'],
+                             size=param['size'],
                              alpha=param['alpha'],
                              edgecolor=None,
-                             s=param['msize'],
+                             s=param['width'] if param['width'] else 16,
                              palette=cmap)
+
+    elif 'hist' in param['ptype']:
+        ax = sns.kdeplot(data=df,
+                x=param['x'],
+                y=param['y'] if param['y'] != 'None' else None,
+                fill=(not param['fill']),
+                stat=param['stat'] if param['stat'] else 'count',
+                bins=param['levels'],
+                hue=param['hue'],
+                kde=('kde' in param['ptype']),
+                multiple=param['multiple'],
+                palette=cmap)
+
+    elif 'kde' in param['ptype']:
+        ax = sns.kdeplot(data=df,
+                x=param['x'],
+                y=param['y'] if param['y'] != 'None' else None,
+                fill=param['fill'],
+                levels=param['levels'],
+                hue=param['hue'],
+                lw=param['width'] if param['width'] else 2,
+                multiple=param['multiple'],
+                palette=cmap)
+
 
     plt.xlabel(param['xlabel'])
     plt.ylabel(param['ylabel'])
