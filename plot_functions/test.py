9a8b8250a8d33fd64c38e05b9563614370908717
Zephan M. Enciso <zenciso@nd.edu>
2022-03-22 15:33:55 -0400

    Add debug printing

--- plot_functions/test.py
+++ plot_functions/test.py
@@ -1,6 +1,8 @@
 # Zephan M. Enciso
 # Intelligent MicroSystems Lab
 
+from pprint import pprint
+
 
 def usage():
     print('Returns the DataFrame for inspection')
@@ -8,3 +10,9 @@ def usage():
 
 def plot(df, kwargs):
     return df
+
+
+def debug(param, df):
+    pprint(param)
+    print()
+    pprint(df)
