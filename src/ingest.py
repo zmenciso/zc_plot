880b47d6f1d7206d85157a49405110c1977cd07e
Zephan M. Enciso <zenciso@nd.edu>
2022-08-19 17:42:18 -0400

    Add MC ingest

--- src/ingest.py
+++ src/ingest.py
@@ -4,6 +4,8 @@
 import pandas as pd
 import numpy as np
 import re
+import sys
+import os
 from src import tools
 
 
@@ -15,6 +17,9 @@ def ingest_wave(filename):
 
     num = len(np.unique([item.split()[0] for item in df_in.columns]))
     series = len(df_in.columns) // 2 // num
+
+    assert series
+
     df = pd.DataFrame(np.tile(np.array(x).astype(float), series).T,
                       columns=['x'])
 
@@ -63,3 +68,42 @@ def ingest_summary(filename):
         df = pd.concat([df, d_fill], axis=1)
 
     return df.dropna(axis=1, how='all')
+
+
+def preprocess(infile, outfile):
+    tools.check_filetype(infile)
+
+    fin = open(infile)
+    columns = fin.readline().split(',')
+
+    index = re.search(r"([0:9])", columns[0]).group(0)
+    count = 1
+    labels = list()
+
+    for col in columns:
+        if (sig := re.search(r"([0-9]+)", col).group(1)) != index:
+            index = sig
+            count = 2
+        else:
+            count = count + 1
+
+        labels.append(f"{col.split()[0]}-{count // 2} {col.split()[1]}")
+
+    fout = open(outfile, 'w')
+    fout.write(f'{",".join(labels)}\n')
+
+    while line := fin.readline():
+        fout.write(line)
+
+def ingest_wave_mc(filename):
+    temp_file = f'temp_{os.path.basename(filename)}'
+    preprocess(filename, temp_file)
+
+    df = ingest_wave(temp_file)
+
+    try:
+        os.remove(temp_file)
+    except Exception as e:
+        print(f'ERROR: Could not remove temporary file {temp_file} ({e})', file=sys.stderr)
+
+    return df
