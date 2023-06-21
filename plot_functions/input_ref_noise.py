05592a298bf58be3aa338784501682cc12744b1b
Zephan M. Enciso <zmenciso@pm.me>
2023-06-21 14:15:43 -0400

    Input Ref Noise: Remove conflicting parameter `bins`

--- plot_functions/input_ref_noise.py
+++ plot_functions/input_ref_noise.py
@@ -17,6 +17,8 @@ def usage():
     fs=float        Set sample rate in Hz (default: 50e6)
     Ts=float        Set sample period in s, overrides fs (default: 1/fs)
     delay=float     Set delay before first sample (default: 0)
+    y=str           Comparator signal (default: Column 1)
+    var=str         Variable to compare against (default: Column 2)
     Uses the same plotting kwargs as replot''')
 
 
@@ -38,7 +40,7 @@ def plot(df, kwargs):
         'fs': 50e6,
         'Ts': None,
         'y': df.columns[1],
-        'bins': df.columns[2],
+        'var': df.columns[2],
         'delay': 9e-9
     }
 
@@ -61,7 +63,7 @@ def plot(df, kwargs):
         time = param['Ts'] + param['delay']
         samples = list()
         for index, series in df.iloc[i:i + size].iterrows():
-            hue = series[param['bins']]
+            hue = series[param['var']]
             if series['x'] >= time:
                 samples.append(series[param['y']])
                 time += param['Ts']
