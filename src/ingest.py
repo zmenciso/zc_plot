5eb3519d675cc1ebcc5479b516e534a009bf26a2
Zephan M. Enciso <zmenciso@pm.me>
2023-07-13 11:30:35 -0400

    Ingest: Support kilo in summary parameters

--- src/ingest.py
+++ src/ingest.py
@@ -4,14 +4,14 @@
 import pandas as pd
 import numpy as np
 import re
-import sys
 import os
-from src import tools
-from src import text
+
+from src.tools import preprocess, check_filetype, si_convert
+from src.text import error
 
 
 def ingest_wave(filename):
-    tools.check_filetype(filename)
+    check_filetype(filename)
 
     df_in = pd.read_csv(filename)
     x = df_in.iloc[:, 0]
@@ -48,15 +48,15 @@ def ingest_wave(filename):
 
 
 def ingest_summary(filename):
-    tools.check_filetype(filename)
+    check_filetype(filename)
 
     df_in = pd.read_csv(filename)
     param = df_in.loc[df_in["Point"].str.contains("Parameters"), "Point"]
 
     df = pd.DataFrame(
-        param.str.findall(r"[0-9a-z\.-]+=([0-9a-z\.-]*)").to_list())
+        param.str.findall(r"[0-9a-zA-Z\.-]+=([0-9a-zA-Z\.-]*)").to_list())
 
-    df = tools.si_convert(df, re.findall(r"([0-9a-z\.-]+)=[0-9a-z\.-]+", param[0]))
+    df = si_convert(df, re.findall(r"([0-9a-z\.-]+)=[0-9a-z\.-]+", param[0]))
     df = df.astype(float)
     outputs = df_in.loc[df_in["Point"] == "1", "Output"]
 
@@ -71,36 +71,6 @@ def ingest_summary(filename):
     return df.dropna(axis=1, how='all')
 
 
-def preprocess(infile, outfile):
-    tools.check_filetype(infile)
-
-    fin = open(infile)
-    columns = fin.readline().split(',')
-
-    index = re.search(r"([0:9])", columns[0]).group(0)
-    count = 1
-    labels = list()
-
-    for col in columns:
-        if (sig := re.search(r"([0-9]+)", col).group(1)) != index:
-            index = sig
-            count = 2
-        else:
-            count = count + 1
-
-        labels.append(f"{col.split()[0]}-{count // 2} {col.split()[1]}")
-
-    try:
-        fout = open(outfile, 'w')
-    except Exception as e:
-        text.error(f'Could not open {outfile} for writing ({e})', 255)
-
-    fout.write(f'{",".join(labels)}\n')
-
-    while line := fin.readline():
-        fout.write(line)
-
-
 def ingest_wave_mc(filename):
     temp_file = f'temp_{os.path.basename(filename)}'
     preprocess(filename, temp_file)
@@ -110,6 +80,6 @@ def ingest_wave_mc(filename):
     try:
         os.remove(temp_file)
     except Exception as e:
-        text.error(f'Could not remove temporary file {temp_file} ({e})')
+        error(f'Could not remove temporary file {temp_file} ({e})')
 
     return df
