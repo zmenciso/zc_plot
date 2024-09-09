bb06deeb52e777ee42538290f6a01854d43265f9
Zephan Enciso <zmenciso@pm.me>
2024-09-09 15:32:01 -0400

    Replot: Global PARAM

--- plot_functions/replot.py
+++ plot_functions/replot.py
@@ -14,6 +14,43 @@ import os
 import warnings
 
 
+PARAM = {
+    'figsize': '6.5,3',
+    'alpha': 0.8,
+    'logy': 'F',
+    'logx': 'F',
+    'logh': 'F',
+    'bbox': 'right',
+    'filetype': 'svg',
+    'filename': None,
+    'ltitle': None,
+    'xlabel': None,
+    'ylabel': None,
+    'xlim': None,
+    'ylim': None,
+    'vlim': None,
+    'axes': None,
+    'context': 'notebook',
+    'palette': 'crest',
+    'x': 'x',
+    'y': 'y',
+    'hue': None,
+    'style': None,
+    'size': None,
+    'ptype': 'line',
+    'width': None,
+    'ci': 95,
+    'bins': 10,
+    'fill': False,
+    'stat': None,
+    'multiple': 'layer',
+    'xscale': 1,
+    'yscale': 1,
+    'hscale': 1,
+    'sscale': 1
+}
+
+
 def usage():
     print(f'{sys.argv[0]} [options] replot INPUT [kwargs]')
     print('''
@@ -36,7 +73,7 @@ Figure
     context=str     cx=str      Scale plot elements (default: 'notebook')
     logx=bool       lx=bool     Enable/disable log for x-axis (default: False)
     logy=bool       ly=bool     Enable/disable log for y-axis (default: False)
-    logv=bool                   Enable/disable log for heatmap vlim (default: False)
+    logh=bool                   Enable/disable log for hue / heatmap vlim (default: False)
     bbox=str        bb=str      Bbox pos (right/center/inside/none, default: 'right')
     xlim=tuple                  Change xlim (default: full range)
     ylim=tuple                  Change ylim (default: full range)
@@ -61,92 +98,96 @@ Spaces in expression/signal names are not supported!''')
 def draw(y, df, cmap):
     warnings.filterwarnings("ignore")
 
-    if 'joint' in param['ptype']:
+    if 'joint' in PARAM['ptype']:
         ax = sns.jointplot(data=df,
-                           x=param['x'],
+                           x=PARAM['x'],
                            y=y,
-                           kind=re.sub('joint', '', param['ptype']),
-                           height=param['figsize'][0],
-                           hue=param['hue'],
+                           kind=re.sub('joint', '', PARAM['ptype']),
+                           height=PARAM['figsize'][0],
+                           hue=PARAM['hue'],
+                           hue_norm=LogNorm() if PARAM['logh'] else None,
                            palette=cmap,
                            marginal_ticks=True)
 
-    elif 'scatter' in param['ptype']:
+    elif 'scatter' in PARAM['ptype']:
         ax = sns.scatterplot(data=df,
-                             x=param['x'],
+                             x=PARAM['x'],
                              y=y,
-                             hue=param['hue'],
-                             style=param['style'],
-                             size=param['size'],
-                             alpha=param['alpha'],
+                             hue=PARAM['hue'],
+                             hue_norm=LogNorm() if PARAM['logh'] else None,
+                             style=PARAM['style'],
+                             size=PARAM['size'],
+                             alpha=PARAM['alpha'],
                              edgecolor=None,
-                             s=param['width'] if param['width'] else 16,
+                             s=PARAM['width'] if PARAM['width'] else 16,
                              palette=cmap)
 
-    if 'line' in param['ptype']:
+    if 'line' in PARAM['ptype']:
         ax = sns.lineplot(data=df,
-                          x=param['x'],
+                          x=PARAM['x'],
                           y=y,
-                          hue=param['hue'],
-                          style=param['style'],
-                          size=param['size'],
-                          alpha=param['alpha'],
-                          estimator=param['stat'] if param['stat'] else 'mean',
-                          lw=param['width'] if param['width'] else 2,
-                          errorbar=('ci', param['ci']),
+                          hue=PARAM['hue'],
+                          hue_norm=LogNorm() if PARAM['logh'] else None,
+                          style=PARAM['style'],
+                          size=PARAM['size'],
+                          alpha=PARAM['alpha'],
+                          estimator=PARAM['stat'] if PARAM['stat'] else 'mean',
+                          lw=PARAM['width'] if PARAM['width'] else 2,
+                          errorbar=('ci', PARAM['ci']),
                           palette=cmap)
 
-    elif 'heat' in param['ptype']:
+    elif 'heat' in PARAM['ptype']:
         # TODO: Rounding is cringe, remove it
-        if not param['hue'] or param['size'] or param['style']:
+        if not PARAM['hue'] or PARAM['size'] or PARAM['style']:
             text.error('heatmap must have only x, y, and hue defined', 350)
 
-        if param['vlim']:
-            param['vlim'] = tuple(map(float, param['vlim'].strip('()').split(',')))
+        if PARAM['vlim']:
+            PARAM['vlim'] = tuple(map(float, PARAM['vlim'].strip('()').split(',')))
         else:
-            param['vlim'] = (None, None)
+            PARAM['vlim'] = (None, None)
 
-        df[param['x']] = np.round(df[param['x']], 1)
+        df[PARAM['x']] = np.round(df[PARAM['x']], 1)
         df[y] = np.round(df[y], 1)
-        df = df.pivot_table(columns=param['x'], index=y, values=param['hue'])
+        df = df.pivot_table(columns=PARAM['x'], index=y, values=PARAM['hue'])
 
-        if param['logv']:
+        if PARAM['logh']:
             ax = sns.heatmap(data=df,
                              cmap=cmap,
                              robust=True,
                              annot=True,
-                             norm=LogNorm() if param['logv'] else None)
+                             norm=LogNorm() if PARAM['logh'] else None)
         else:
             ax = sns.heatmap(data=df,
                              cmap=cmap,
                              robust=True,
                              annot=True,
-                             vmin=param['vlim'][0],
-                             vmax=param['vlim'][1])
+                             vmin=PARAM['vlim'][0],
+                             vmax=PARAM['vlim'][1])
 
-    if 'hist' in param['ptype']:
+    if 'hist' in PARAM['ptype']:
         ax = sns.histplot(data=df,
-                          x=param['x'],
+                          x=PARAM['x'],
                           y=y if y.lower() != 'none' else None,
-                          fill=not param['fill'],
-                          stat=param['stat'] if param['stat'] else 'count',
-                          bins=param['bins'],
-                          hue=param['hue'],
-                          kde=('kde' in param['ptype']),
-                          multiple=param['multiple'],
+                          fill=not PARAM['fill'],
+                          stat=PARAM['stat'] if PARAM['stat'] else 'count',
+                          bins=PARAM['bins'],
+                          hue=PARAM['hue'],
+                          kde=('kde' in PARAM['ptype']),
+                          multiple=PARAM['multiple'],
                           cbar=True if y.lower() != 'none' else False,
                           cbar_kws=dict(shrink=.75) if y.lower() != 'none' else None,
                           palette=cmap)
 
-    elif 'kde' in param['ptype']:
+    elif 'kde' in PARAM['ptype']:
         ax = sns.kdeplot(data=df,
-                         x=param['x'],
+                         x=PARAM['x'],
                          y=y if y.lower() != 'none' else None,
-                         fill=param['fill'],
-                         levels=param['bins'],
-                         hue=param['hue'],
-                         lw=param['width'] if param['width'] else 2,
-                         multiple=param['multiple'],
+                         fill=PARAM['fill'],
+                         levels=PARAM['bins'],
+                         hue=PARAM['hue'],
+                         hue_norm=LogNorm() if PARAM['logh'] else None,
+                         lw=PARAM['width'] if PARAM['width'] else 2,
+                         multiple=PARAM['multiple'],
                          cbar=True if y != 'None' else False,
                          cbar_kws=dict(shrink=.75) if y != 'None' else None,
                          palette=cmap)
@@ -155,59 +196,59 @@ def draw(y, df, cmap):
 
 
 def draw_legend():
-    if param['bbox'] == 'none' or 'heat' in param['ptype'] or not param['ltitle']:
+    if PARAM['bbox'] == 'none' or 'heat' in PARAM['ptype'] or not PARAM['ltitle']:
         return
 
     handles, labels = plt.gca().get_legend_handles_labels()
 
-    if len(param['y']) > 1:
-        r = len(labels) // len(param['y'])
-        for index, wave in enumerate(param['y']):
+    if len(PARAM['y']) > 1:
+        r = len(labels) // len(PARAM['y'])
+        for index, wave in enumerate(PARAM['y']):
             for i in range(index * r, index * r + r):
                 labels[i] = f'{wave} ' + labels[i]
 
-    if param['bbox'] == 'center':
+    if PARAM['bbox'] == 'center':
         plt.legend(handles,
                    labels,
                    loc='upper center',
                    bbox_to_anchor=(.5, 1.25),
                    ncol=len(handles),
-                   title=param['ltitle']
-                   if param['ltitle'].lower() != 'none' else None,
+                   title=PARAM['ltitle']
+                   if PARAM['ltitle'].lower() != 'none' else None,
                    borderaxespad=0)
 
     else:
         plt.legend(handles,
                    labels,
-                   loc='upper left' if param['bbox'] != 'inside' else None,
-                   bbox_to_anchor=(1.02, 1) if param['bbox'] != 'inside' else None,
-                   title=param['ltitle']
-                   if param['ltitle'].lower() != 'none' else None,
+                   loc='upper left' if PARAM['bbox'] != 'inside' else None,
+                   bbox_to_anchor=(1.02, 1) if PARAM['bbox'] != 'inside' else None,
+                   title=PARAM['ltitle']
+                   if PARAM['ltitle'].lower() != 'none' else None,
                    borderaxespad=0)
 
 
 def draw_labels(ax):
     # Label axes
-    if param['xlabel'].lower() == 'none':
+    if PARAM['xlabel'].lower() == 'none':
         plt.xlabel(None)
     else:
-        plt.xlabel(param['xlabel'])
+        plt.xlabel(PARAM['xlabel'])
 
-    if param['ylabel'].lower() == 'none':
+    if PARAM['ylabel'].lower() == 'none':
         plt.ylabel(None)
     else:
-        plt.ylabel(param['ylabel'])
+        plt.ylabel(PARAM['ylabel'])
 
     # Set axes limits
-    if param['xlim']:
-        param['xlim'] = tuple(map(float, param['xlim'].strip('()').split(',')))
-        plt.xlim(param['xlim'])
-    if param['ylim']:
-        param['ylim'] = tuple(map(float, param['ylim'].strip('()').split(',')))
-        plt.ylim(param['ylim'])
-    if param['logx']:
+    if PARAM['xlim']:
+        PARAM['xlim'] = tuple(map(float, PARAM['xlim'].strip('()').split(',')))
+        plt.xlim(PARAM['xlim'])
+    if PARAM['ylim']:
+        PARAM['ylim'] = tuple(map(float, PARAM['ylim'].strip('()').split(',')))
+        plt.ylim(PARAM['ylim'])
+    if PARAM['logx']:
         ax.set_xscale('log')
-    if param['logy']:
+    if PARAM['logy']:
         ax.set_yscale('log')
 
     return ax
@@ -241,135 +282,102 @@ def key_expander(key):
     return conversion[key] if key in conversion else key
 
 
-def augment_param():
+def augment_PARAM():
     # Fix data types
-    param['y'] = re.sub(r'\s+', '', param['y']).strip('{[()]}').split(',')
-    param['palette'] = re.sub(r'\s+', '', param['palette']).strip('{[()]}').split(',')
-    param['figsize'] = tuple(
-        map(float, param['figsize'].strip('{[()]}').split(',')))
-    param['alpha'] = float(param['alpha'])
-    param['width'] = float(param['width']) if param['width'] else None
-    param['ci'] = float(param['ci'])
-    param['logy'] = param['logy'].lower() in ['t', 'true', 'yes', 'y', '1']
-    param['logx'] = param['logx'].lower() in ['t', 'true', 'yes', 'y', '1']
-    param['logv'] = param['logv'].lower() in ['t', 'true', 'yes', 'y', '1']
-    param['fill'] = bool(param['fill'])
-    param['bins'] = int(param['bins'])
-    param['xscale'] = float(param['xscale'])
-    param['yscale'] = float(param['yscale'])
-    param['hscale'] = float(param['hscale'])
-    param['sscale'] = float(param['sscale'])
-    param['ptype'] = param['ptype'].lower()
+    PARAM['y'] = re.sub(r'\s+', '', PARAM['y']).strip('{[()]}').split(',')
+    PARAM['palette'] = re.sub(r'\s+', '', PARAM['palette']).strip('{[()]}').split(',')
+    PARAM['figsize'] = tuple(
+        map(float, PARAM['figsize'].strip('{[()]}').split(',')))
+    PARAM['alpha'] = float(PARAM['alpha'])
+    PARAM['width'] = float(PARAM['width']) if PARAM['width'] else None
+    PARAM['ci'] = float(PARAM['ci'])
+    PARAM['logy'] = PARAM['logy'].lower() in ['t', 'true', 'yes', 'y', '1']
+    PARAM['logx'] = PARAM['logx'].lower() in ['t', 'true', 'yes', 'y', '1']
+    PARAM['logh'] = PARAM['logh'].lower() in ['t', 'true', 'yes', 'y', '1']
+    PARAM['fill'] = bool(PARAM['fill'])
+    PARAM['bins'] = int(PARAM['bins'])
+    PARAM['xscale'] = float(PARAM['xscale'])
+    PARAM['yscale'] = float(PARAM['yscale'])
+    PARAM['hscale'] = float(PARAM['hscale'])
+    PARAM['sscale'] = float(PARAM['sscale'])
+    PARAM['ptype'] = PARAM['ptype'].lower()
 
-    if param['ylim'] and 'none' in param['ylim'].lower():
-        param['ylim'] = None
-    if param['xlim'] and 'none' in param['xlim'].lower():
-        param['xlim'] = None
-    if param['vlim'] and 'none' in param['vlim'].lower():
-        param['vlim'] = None
+    if PARAM['ylim'] and 'none' in PARAM['ylim'].lower():
+        PARAM['ylim'] = None
+    if PARAM['xlim'] and 'none' in PARAM['xlim'].lower():
+        PARAM['xlim'] = None
+    if PARAM['vlim'] and 'none' in PARAM['vlim'].lower():
+        PARAM['vlim'] = None
 
     # Set labels
-    if not param['ylabel']:
-        param['ylabel'] = param['y'][0] \
-            if 'hist' not in param['ptype'] or 'kde' not in param['ptype'] \
+    if not PARAM['ylabel']:
+        PARAM['ylabel'] = PARAM['y'][0] \
+            if 'hist' not in PARAM['ptype'] or 'kde' not in PARAM['ptype'] \
             else 'none'
 
-    if not param['xlabel']:
-        param['xlabel'] = param['x']
+    if not PARAM['xlabel']:
+        PARAM['xlabel'] = PARAM['x']
 
     # Set legend title
-    if not param['ltitle'] and param['hue']:
-        param['ltitle'] = f"{param['hue']}/{param['style']}" if param[
-            'style'] else param['hue']
-    elif not param['ltitle'] and len(param['y']) > 1:
+    if not PARAM['ltitle'] and PARAM['hue']:
+        PARAM['ltitle'] = f"{PARAM['hue']}/{PARAM['style']}" if PARAM[
+            'style'] else PARAM['hue']
+    elif not PARAM['ltitle'] and len(PARAM['y']) > 1:
         # TODO: Create legend for multiple y series without hue or style
-        # param['ltitle'] = 'Series'
-        param['ltitle'] = None
+        # PARAM['ltitle'] = 'Series'
+        PARAM['ltitle'] = None
 
-    return param
+    return PARAM
 
 
 def rescale(df):
-    if param['xscale'] != 1:
-        df[param['x']] = df[param['x']] * param['xscale']
-    if param['yscale'] != 1:
-        df[param['y']] = df[param['y']] * param['yscale']
-    if param['hscale'] != 1:
-        df[param['hue']] = df[param['hue']] * param['hscale']
-    if param['sscale'] != 1:
-        df[param['size']] = df[param['size']] * param['sscale']
+    if PARAM['xscale'] != 1:
+        df[PARAM['x']] = df[PARAM['x']] * PARAM['xscale']
+    if PARAM['yscale'] != 1:
+        df[PARAM['y']] = df[PARAM['y']] * PARAM['yscale']
+    if PARAM['hscale'] != 1:
+        df[PARAM['hue']] = df[PARAM['hue']] * PARAM['hscale']
+    if PARAM['sscale'] != 1:
+        df[PARAM['size']] = df[PARAM['size']] * PARAM['sscale']
 
     return df
 
 
 def plot(df, kwargs):
-    global param
-    param = {
-        'figsize': '6.5,3',
-        'alpha': 0.8,
-        'logy': 'F',
-        'logx': 'F',
-        'logv': 'F',
-        'bbox': 'right',
-        'filetype': 'svg',
-        'filename': None,
-        'ltitle': None,
-        'xlabel': None,
-        'ylabel': None,
-        'xlim': None,
-        'ylim': None,
-        'vlim': None,
-        'axes': None,
-        'context': 'notebook',
-        'palette': 'crest',
-        'x': df.columns[0],
-        'y': df.columns[1],
-        'hue': None,
-        'style': None,
-        'size': None,
-        'ptype': 'line',
-        'width': None,
-        'ci': 95,
-        'bins': 10,
-        'fill': False,
-        'stat': None,
-        'multiple': 'layer',
-        'xscale': 1,
-        'yscale': 1,
-        'hscale': 1,
-        'sscale': 1
-    }
+    global PARAM
+    PARAM['x'] = df.columns[0]
+    PARAM['y'] = df.columns[1]
 
     # Read kwargs
     for arg in kwargs:
         try:
             key, value = arg.split('=')
             key = key_expander(key)
-            param[key] = value
+            PARAM[key] = value
         except Exception as e:
             text.error(f'Unable to decode arg {arg} ({e})')
 
-    # Fix param variable types
-    param = augment_param()
+    # Fix PARAM variable types
+    PARAM = augment_PARAM()
 
     # Set figure size and type
-    plt.figure(figsize=param['figsize'])
-    if not param['axes']:
+    plt.figure(figsize=PARAM['figsize'])
+    if not PARAM['axes']:
         sns.set_style('darkgrid', {'axes.facecolor': '#ebebeb'})
     else:
-        sns.set_style(param['axes'])
-    sns.set_context(param['context'])
+        sns.set_style(PARAM['axes'])
+    sns.set_context(PARAM['context'])
 
     # Change hue column to floats
-    if param['hue']:
-        df[param['hue']] = df[param['hue']].astype(float)
+    if PARAM['hue']:
+        df[PARAM['hue']] = df[PARAM['hue']].astype(float)
 
     # Rescale (avoid multiplication duplication if possible)
     df = rescale(df)
 
     # Draw plots!
-    for index, y in enumerate(param['y']):
-        palette = param['palette'][index % len(param['palette'])]
+    for index, y in enumerate(PARAM['y']):
+        palette = PARAM['palette'][index % len(PARAM['palette'])]
         cmap = sns.color_palette(palette, as_cmap=True)
         ax = draw(y, df, cmap)
 
@@ -379,11 +387,11 @@ def plot(df, kwargs):
     plt.tight_layout()
 
     # Write out
-    if param['filename']:
-        filename = param['filename'] + '.' + param['filetype']
+    if PARAM['filename']:
+        filename = PARAM['filename'] + '.' + PARAM['filetype']
     else:
-        y = re.sub('/', '-', '+'.join(param['y']))
-        filename = f'./plots/{y}_' + f"{param['time']}.{param['filetype']}"
+        y = re.sub('/', '-', '+'.join(PARAM['y']))
+        filename = f'./plots/{y}_' + f"{PARAM['time']}.{PARAM['filetype']}"
 
     allow = tools.query(f'Overwrite {filename}?', 'yes') \
         if os.path.isfile(filename) else True
