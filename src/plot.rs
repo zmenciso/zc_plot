5eb7879c7b22879207c8a64d695dd8b6e494e6f7
Zephan Enciso <zmenciso@pm.me>
2024-10-17 16:03:05 -0400

    Initial import

---
+++ src/plot.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use charming::Chart;
+use charming::component;
+use charming::ImageRenderer;
+
+use crate::ingest::DataFrame;
+use crate::options::Options;
+
+pub fn plot(df: DataFrame, options: Options) -> Result<(), Box<dyn Error>> {
+    let mut chart: Chart = match options.retrieve(String::from("axes")).as_str() {
+        _ => {
+            Chart::new()
+                .grid(component::Grid::new())
+                .x_axis(component::Axis::new())
+                .y_axis(component::Axis::new())
+        }
+    };
+
+    match options.retrieve(String::from("series")).as_str() {
+        "line" => { line(df, options, &mut chart); },
+        _ => { line(df, options, &mut chart); },
+    }
+
+    Ok(())
+}
+
+fn line(df: DataFrame, options: Options, chart: &mut Chart) {
+
+    // TODO: Catch errors here
+    let mut renderer = ImageRenderer::new(1000, 800);
+    renderer.save(&chart, options.retrieve(String::from("filename"))).unwrap();
+}
